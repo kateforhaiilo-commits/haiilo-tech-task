1d45488c09abf29a0d6ae3b263dbd392ce0e2bc6
theMetalkate <>
2025-11-20 13:21:28 +0100

    feat(items): add items
    * add mockdata to show items
    * add price with currency pipe
    * add input for amount
    
    Refs: FE-01

--- frontend/src/app/items/items.ts
+++ frontend/src/app/items/items.ts
@@ -1,11 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'items',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './items.html',
   styleUrl: './items.scss',
 })
 export class ItemsComponent {
-
+  public items = [{
+    name: 'apple',
+    price: 1.2
+  }, {
+    name: 'banana',
+    price: 2.3
+  }, {
+    name: 'orange',
+    price: 3.4
+  }];
 }
